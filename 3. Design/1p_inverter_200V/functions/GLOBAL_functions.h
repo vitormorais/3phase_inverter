aca52f315246b4afaaa5be0b1bfa0aa5ccb71e6d
Vítor Morais <vitor.fa.morais@gmail.com>
2020-01-07 09:33:35 +0000

    hardware: minor_changes; tuned 200V DC-bus; started 400V tuning.

---
+++ 3. Design/1p_inverter_200V/functions/GLOBAL_functions.h
@@ -0,0 +1,141 @@
+#define CTRL_FREQ 10000
+#define CTRL_PERIOD  1/CTRL_FREQ
+float cycle_time = 0;
+// math constants
+#define PI_const  3.1415926535897932384626433832795028841971693993751f
+#define PI32 4.7123890f			// 3/2*PI
+#define PI2 6.2831853f			// 2*PI
+#define K23 0.66666667f			// 2/3
+#define Ksqr32 0.86602540f		// sqrt(3)/2
+#define K23sqr3 0.38490018f		// 2/(3*sqrt(3))
+
+// system constants
+#define fs 5000.000f
+#define Ts 0.00010000f
+#define wL 3.3600000f			// =100*pi*10.7e-3 //=100*pi*(1.25e-3*2) //=100*pi*5.2e-3
+#define R 2.5600000f
+#define Kp_PLL 1.370000f
+#define Ki_PLL 163.00000f
+#define Kp_I 2.5600000f
+#define Ki_I 1225.0000f
+#define kd 0.00100000f
+#define td 0.06600000f
+#define Kp_V_DC 0.0005 //0.311111f
+#define Ki_V_DC 0.03 //20.1707f
+#define TIMER_DELAY_INIT 1*1000000u		//Scaling the required values by 100000.
+#define TIMER_K1 4*1000000u				//Scaling the required values by 100000.
+#define TIMER_K2 10*1000000u			//Scaling the required values by 100000.
+#define TIMER_OPEN_K1_K2 5*1000000u		//Scaling the required values by 100000.
+#define ma_max 0.970000f				// Maximum modulation index
+int chargeTime=2000000;
+
+// 1st order filters constants
+#define wo_LPF_1 6.28319f		// LPF cutoff frequency- 1 Hz
+#define wo_LPF_10 78.5398f		// LPF cutoff frequency- 12.5 Hz
+#define wo_LPF_50 314.159f		// LPF cutoff frequency- 50 Hz
+#define wo_LPF_100 628.319f		// LPF cutoff frequency- 100 Hz
+#define wo_LPF_500 3455.75f		// LPF cutoff frequency- 550 Hz (3455.75f)
+#define wo_LPF_1500 9424.78f	// LPF cutoff frequency- 1500 Hz
+#define wo_LPF_Iref 314.159f	// LPF cutoff frequency- 50 Hz
+
+const float K_sync[4]={50000.0f, 0.75f, 10.0f};
+const float PI_K_CC[2]={Kp_I, Ki_I};
+const float HPF_K[4]={kd*td, -kd*td, td, td-Ts};
+// LPF_K_X[3]={k, a0/b1, -b0/b1}
+const float LPF_K_1[3]={1, Ts*wo_LPF_1/(2+Ts*wo_LPF_1), -(Ts*wo_LPF_1-2)/(2+Ts*wo_LPF_1)};
+const float LPF_K_10[3]={1, Ts*wo_LPF_10/(2+Ts*wo_LPF_10), -(Ts*wo_LPF_10-2)/(2+Ts*wo_LPF_10)};
+const float LPF_K_50[3]={1, Ts*wo_LPF_50/(2+Ts*wo_LPF_50), -(Ts*wo_LPF_50-2)/(2+Ts*wo_LPF_50)};
+const float LPF_K_100[3]={1, Ts*wo_LPF_100/(2+Ts*wo_LPF_100), -(Ts*wo_LPF_100-2)/(2+Ts*wo_LPF_100)};
+const float LPF_K_500[3]={1, Ts*wo_LPF_500/(2+Ts*wo_LPF_500), -(Ts*wo_LPF_500-2)/(2+Ts*wo_LPF_500)};
+const float LPF_K_1500[3]={1, Ts*wo_LPF_1500/(2+Ts*wo_LPF_1500), -(Ts*wo_LPF_1500-2)/(2+Ts*wo_LPF_1500)};
+
+
+////////// adc
+
+float V_DC=0.0f;					// DC bus voltage
+float V_abc[3]={0.0f, 0.0f, 0.0f};	// AC voltages
+float I_abc[3]={0.0f, 0.0f, 0.0f};	// AC currents
+float conv_temp_C = 0.0f;
+
+
+//////////  BUFFERS
+float LPFbuff_vdc[2]={0.0f,0.0f};
+float LPFbuff_vu[2]={0.0f,0.0f};
+//float LPFbuff_vdc
+
+
+/////////  STRUCTS
+
+typedef struct{
+	float Idq[2];
+	float PQ[2];
+	float Vdc;
+	float Vdq[2];
+	float w;
+	float wt;
+	float temp;
+}ConverterVars;
+
+ConverterVars conv;//={{0.0f, 0.0f},{0.0f, 0.0f}, 0.0f, {0.0f, 0.0f}, 0.0f, 0.0f, 0.0f};
+
+typedef struct{
+	float K[2]; 	// Kp Ki
+	float sat[2];		// output saturation
+	float buff[2];		// (x[n]; yi[n])
+	int flag;
+}PI_tr_p;
+
+PI_tr_p PI_PLL;//={{Kp_PLL, Ki_PLL},{942.47780f, -942.47780f},{0.0f, 0.0f},0};
+PI_tr_p PI_Vdc;//={{Kp_V_DC, Ki_V_DC},{15, -15},{0.0f, 0.0f},0u};
+
+
+float LPF1(float val, float *LPFbuff, const float LPF_K[3]);
+float fw_integ(float val, float *buff, float Ti, float rst);
+//float PI_TR_par (float error, float Ti, PI_tr_p *pi);
+
+
+float LPF1(float val, float *LPFbuff, const float LPF_K[3])  {
+	// y[k]= y[k-1]*(-b0/b1)+a0/b1*(x[k]+x[k-1])
+	float out = (*(LPF_K+2)) * (*(LPFbuff+1)) + (*LPF_K) * (*(LPF_K+1)) * (val+(*LPFbuff));
+	*LPFbuff = val;		// x[k-1]
+	*(LPFbuff+1) = out;	// y[k-1]
+	return out;
+}
+
+float fw_integ(float val, float *buff, float Ti, float rst){
+	float out=*buff+val*Ti;
+	if (rst!=0.0f){
+		if (out>rst){
+			out=out-rst+out*Ti;
+		}
+		else if (out<0.0f){
+			out=out+rst-out*Ti;
+		}
+	}
+	*buff=out;
+	return out;
+}
+
+float PI_TR_par (float error, float Ti, PI_tr_p *pi){
+	// bilinear discretization PI in parallel form:
+	// to use in ideal form, replace Ki -> Kp*Ki
+	float u=0.0f;
+	// check if output is not saturated
+	if (pi->flag==0u){
+		pi->buff[1]+=(error+pi->buff[0])*0.5f*Ti;
+	}
+	u=pi->K[0]*error+pi->K[1]*pi->buff[1];
+	if (u > pi->sat[0]){
+		u=pi->sat[0];
+		pi->flag=pi->flag|0b00000001;
+	}
+	else if (u < pi->sat[1]){
+		u=pi->sat[1];
+		pi->flag=pi->flag|0b00000001;
+	}
+	else{
+		pi->flag=pi->flag&0b1111110;
+	}
+	pi->buff[0]=error;
+	return u;
+}
---
+++ 3. Design/1p_inverter_400V/dc_ac_1ph_code.cpp
@@ -0,0 +1,237 @@
+
+#include <Stdlib.h>
+#include <String.h>
+#include <math.h>
+#include <Psim.h>
+
+#include "functions/GLOBAL_functions.h"
+#include "functions/SM_functions.h"
+#include "functions/CTRL_functions.h"
+#include "functions/PLL_functions.h"
+
+#define PSIM_IN_SCADA 0
+#define PSIM_IN_ADC 2
+#define PSIM_IN_REFS 9
+
+#define PSIM_OUT_AUX 		0						// 2 saidas auxiliares
+#define PSIM_OUT_SCADA 		2						// 6 saidas scada
+#define PSIM_OUT_ENABLES	8               		// 5 saidas p/ enables
+#define PSIM_OUT_PLL		13						// 6 saidas p/ pll
+#define PSIM_OUT_CTRL		19						// 8 saidas p/ ctrl
+
+
+//#include "functions.h"
+
+// PLACE GLOBAL VARIABLES OR USER FUNCTIONS HERE...
+
+
+
+
+///   function prototypes, "objects"
+
+float PR_LPFbuff_Iu[2] = {0.0f, 0.0f}; 
+float Iu_filt = 0.0f;
+
+
+
+/////////////////////////////////////////////////////////////////////
+// FUNCTION: SimulationStep
+//   This function runs at every time step.
+//double t: (read only) time
+//double delt: (read only) time step as in Simulation control
+//double *in: (read only) zero based array of input values. in[0] is the first node, in[1] second input...
+//double *out: (write only) zero based array of output values. out[0] is the first node, out[1] second output...
+//int *pnError: (write only)  assign  *pnError = 1;  if there is an error and set the error message in szErrorMsg
+//    strcpy(szErrorMsg, "Error message here..."); 
+// DO NOT CHANGE THE NAME OR PARAMETERS OF THIS FUNCTION
+void SimulationStep(
+	double t, double delt, double *in, double *out,
+	int *pnError, char * szErrorMsg,
+	void ** reserved_UserData, int reserved_ThreadIndex, void * reserved_AppPtr)
+{
+// ENTER YOUR CODE HERE...
+
+SCADA_enable = in[PSIM_IN_SCADA];
+SCADA_disable = in[PSIM_IN_SCADA+1];
+
+
+/////////////////////////////////////////////////////////////////
+if(cycle_time >= (float) CTRL_PERIOD) {
+	/////////////////   PERIODIC CODE
+	///// acquisition ADC's
+	V_abc[0] = in[PSIM_IN_ADC];  // 0.0f;
+	V_abc[1] = in[PSIM_IN_ADC+1];  // 0.0f;
+	V_abc[2] = in[PSIM_IN_ADC+2];  // 0.0f;
+	I_abc[0] = in[PSIM_IN_ADC+3];  // 0.0f;
+	I_abc[1] = in[PSIM_IN_ADC+4];  // 0.0f;
+	I_abc[2] = in[PSIM_IN_ADC+5];  // 0.0f;
+	V_DC = in[PSIM_IN_ADC+6];  // 0.0f;
+	conv.Vdc = LPF1(V_DC, LPFbuff_vdc, LPF_K_500);//V_DC;
+	conv_temp_C = 30.0f;  ///  no input var in PSIM
+	conv.temp = conv_temp_C;
+	///// end acquisition ADC's
+
+	///// begin PLL
+	QSG(V_abc[0], dqsg.w, &(dqsg.QSG_d));
+	SOGI_v_AB[0] = dqsg.QSG_d.v_AB[0];
+	SOGI_v_AB[1] = dqsg.QSG_d.v_AB[1];//*(w_aux/(100.0f*PI));
+	SOGI_v_AB[1] *= (PLL_w / PLL_w0);
+
+	AB_dq(SOGI_v_AB, PLL_Vdq, PLL_wt);
+	PLL_Vdq[0] = LPF1(PLL_Vdq[0], PLL_LPFbuff_d, LPF_K_500);
+	PLL_Vdq[1] = LPF1(PLL_Vdq[1], PLL_LPFbuff_q, LPF_K_500);
+	conv.Vdq[0] = PLL_Vdq[0];
+	conv.Vdq[1] = PLL_Vdq[1];
+
+	PLL_w = PI_TR_par(PLL_Vdq[1], Ts, &PI_PLL) + PLL_w0;
+	PLL_wt = fw_integ(PLL_w, &PLL_wt_buff, Ts, PI2);
+	PLL_w = LPF1(PLL_w, PLL_LPFbuff_w, LPF_K_10);
+	conv.wt = PLL_wt + ((PLL_w*PLL_w - PLL_w0*PLL_w0)/(1.4142136f*PLL_w0*PLL_w));
+	///// end PLL
+
+	///// begin DC_ctrl
+	//run_DC_ctrl();
+    VDC_BUS_REF = 390.0f;
+    if (state == running) {
+    	//vdc_bus_error = ((VDC_BUS_REF)-(conv.Vdc));
+        vdc_bus_error = ((VDC_BUS_REF*VDC_BUS_REF)-(conv.Vdc*conv.Vdc));
+    }
+    else  {
+        vdc_bus_error = 0;
+    }
+
+    I_dq_ref[0] = -PI_TR_par(vdc_bus_error, Ts, &PI_Vdc);//10; //activa
+    I_dq_ref[1] = 0;  //reactiva
+    dq_AB(I_dq_ref, I_AB, conv.wt);
+
+	///// end DC_ctrl
+
+	///// begin PR_ctrl
+	if (state != running) {
+		set_PR_ref(&PR_Test, 0);
+	}
+	set_PR_ref(&PR_Test, I_AB[0]);
+	Iu_filt = LPF1(I_abc[0], PR_LPFbuff_Iu, LPF_K_500);
+	PR_output = Calc_prStruct(&PR_Test, Iu_filt, 100.0f*PI_const) ;
+	if (state != running) {
+		PR_output = 0;
+	}
+
+	PR_output = (PR_output >  20.0f) ?  20.0f : PR_output;
+	PR_output = (PR_output < -20.0f) ? -20.0f : PR_output;
+
+	vdc_division = (conv.Vdc < 100) ? 100 : conv.Vdc;   // permitir divisão por 0
+	m_u = (PR_output + V_abc[0]) / (vdc_division);
+	m_v = -m_u;
+	///// end PR_ctrl
+
+	/////  state machine
+	ConverterStateMachine();
+	/////  end state machine
+
+	/////////////////  END PERIODIC CODE
+out[PSIM_OUT_AUX] = 1;
+cycle_time=0;
+}
+else
+{
+	out[PSIM_OUT_AUX]=0;
+	cycle_time += delt;
+}
+///////////////////////////////////////////////////////////////////////
+out[PSIM_OUT_AUX+1]=state;
+
+out[PSIM_OUT_SCADA+0] = SCADA_error_Ia;			//Flag
+out[PSIM_OUT_SCADA+1] = SCADA_error_Ib;			//Flag
+out[PSIM_OUT_SCADA+2] = SCADA_error_Ic;			//Flag
+out[PSIM_OUT_SCADA+3] = SCADA_error_VDCmax;		//Flag
+out[PSIM_OUT_SCADA+4] = SCADA_error_VDCmin;		//Flag
+out[PSIM_OUT_SCADA+5] = SCADA_error_Tmax;		//Flag
+
+out[PSIM_OUT_ENABLES+0] = EN_1;
+out[PSIM_OUT_ENABLES+1] = EN_2;
+out[PSIM_OUT_ENABLES+2] = EN_3;
+out[PSIM_OUT_ENABLES+3] = PCR_SW;
+out[PSIM_OUT_ENABLES+4] = PCS_SW;
+
+out[PSIM_OUT_PLL+0] = SOGI_v_AB[0];
+out[PSIM_OUT_PLL+1] = SOGI_v_AB[1];
+out[PSIM_OUT_PLL+2] = PLL_Vdq[0];
+out[PSIM_OUT_PLL+3] = PLL_Vdq[1];
+out[PSIM_OUT_PLL+4] = PLL_w;
+out[PSIM_OUT_PLL+5] = PLL_wt;
+
+out[PSIM_OUT_CTRL+0] = in[9];
+out[PSIM_OUT_CTRL+1] = 0;
+out[PSIM_OUT_CTRL+2] = I_dq_ref[0];
+out[PSIM_OUT_CTRL+3] = I_dq_ref[1];
+out[PSIM_OUT_CTRL+4] = I_AB[0];
+out[PSIM_OUT_CTRL+5] = PR_output;
+out[PSIM_OUT_CTRL+6] = m_u;
+out[PSIM_OUT_CTRL+7] = m_v;
+}
+
+
+
+
+/////////////////////////////////////////////////////////////////////
+// FUNCTION: SimulationBegin
+//   Initialization function. This function runs once at the beginning of simulation
+//   For parameter sweep or AC sweep simulation, this function runs at the beginning of each simulation cycle.
+//   Use this function to initialize static or global variables.
+//const char *szId: (read only) Name of the C-block 
+//int nInputCount: (read only) Number of input nodes
+//int nOutputCount: (read only) Number of output nodes
+//int nParameterCount: (read only) Number of parameters is always zero for C-Blocks.  Ignore nParameterCount and pszParameters
+//int *pnError: (write only)  assign  *pnError = 1;  if there is an error and set the error message in szErrorMsg
+//    strcpy(szErrorMsg, "Error message here..."); 
+// DO NOT CHANGE THE NAME OR PARAMETERS OF THIS FUNCTION
+void SimulationBegin(
+	const char *szId, int nInputCount, int nOutputCount,
+	int nParameterCount, const char ** pszParameters,
+	int *pnError, char * szErrorMsg,
+	void ** reserved_UserData, int reserved_ThreadIndex, void * reserved_AppPtr)
+{
+// ENTER INITIALIZATION CODE HERE...
+
+Init_prStruct(&PR_Test, PR_Kp, PR_Kr, PR_Wi);
+//dqsg = {100.0f*PI_const, {{0.0f, 0.0f}, 1.4142136f, 0.0f, 0.0f, Ts}, {{0.0f, 0.0f}, 1.4142136f, 0.0f, 0.0f, Ts}};
+dqsg.w = 100.0f*PI_const;
+dqsg.QSG_d.K = 1.4142136f;
+dqsg.QSG_d.Ti = Ts;
+dqsg.QSG_q.K = 1.4142136f;
+dqsg.QSG_q.Ti = Ts;
+//PI_PLL = {{Kp_PLL, Ki_PLL},{942.47780f, -942.47780f},{0.0f, 0.0f},0};
+PI_PLL.K[0] = Kp_PLL;
+PI_PLL.K[1] = Ki_PLL;
+PI_PLL.sat[0] = 942.47780f;
+PI_PLL.sat[1] = -942.47780f;
+
+//PI_Vdc = {{Kp_V_DC, Ki_V_DC},{15, -15},{0.0f, 0.0f},0u};
+PI_Vdc.K[0] = Kp_V_DC;
+PI_Vdc.K[1] = Ki_V_DC;
+PI_Vdc.sat[0] = 15.0f;
+PI_Vdc.sat[1] = -15.0f;
+
+PLL_LPFbuff_w[0] = PLL_w0;
+PLL_LPFbuff_w[1] = PLL_w0;
+
+}
+
+
+/////////////////////////////////////////////////////////////////////
+// FUNCTION: SimulationEnd
+//   Termination function. This function runs once at the end of simulation
+//   For parameter sweep or AC sweep simulation, this function runs at the end of each simulation cycle.
+//   Use this function to de-allocate any allocated memory or to save the result of simulation in an alternate file.
+// Ignore all parameters for C-block 
+// DO NOT CHANGE THE NAME OR PARAMETERS OF THIS FUNCTION
+void SimulationEnd(const char *szId, void ** reserved_UserData, int reserved_ThreadIndex, void * reserved_AppPtr)
+{
+
+
+
+
+}
+
+
